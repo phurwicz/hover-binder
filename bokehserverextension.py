786497ca1481ffa41b0afeee24a96250f31424bb
phurwicz <pavelhurwicz@gmail.com>
2020-11-17 06:57:37 +0000

    The last version worked; see if we can host on two ports

--- bokehserverextension.py
+++ bokehserverextension.py
@@ -4,4 +4,4 @@ from subprocess import Popen
 def load_jupyter_server_extension(nbapp):
     """serve the bokeh-app directory with bokeh server"""
     Popen(["bokeh", "serve", "simple-annotator", "--port=8890", "--allow-websocket-origin=*"])
-    #Popen(["bokeh", "serve", "linked-annotator", "--port=8891", "--allow-websocket-origin=*"])
+    Popen(["bokeh", "serve", "linked-annotator", "--port=8891", "--allow-websocket-origin=*"])
