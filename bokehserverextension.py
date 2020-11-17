0345298489e12be9a2797da72b8f7a5236b50353
phurwicz <pavelhurwicz@gmail.com>
2020-11-17 04:41:50 -0500

    Bugfix

--- bokehserverextension.py
+++ bokehserverextension.py
@@ -6,7 +6,7 @@ def load_jupyter_server_extension(nbapp):
     Popen([
         "bokeh",
         "serve",
-        "simple-annotator",
-        "linked-annotator",
+        "app-simple-annotator",
+        "app-linked-annotator",
         "--allow-websocket-origin=*",
     ])
