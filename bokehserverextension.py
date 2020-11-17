148279220e36b91745cd69aa8120fb0e6396a4e4
phurwicz <pavelhurwicz@gmail.com>
2020-11-17 03:06:41 -0500

    Improvement: hosting multiple apps on the same Bokeh server at the same port; revised README for better looking and less confusion

--- bokehserverextension.py
+++ bokehserverextension.py
@@ -3,5 +3,10 @@ from subprocess import Popen
 
 def load_jupyter_server_extension(nbapp):
     """serve the bokeh-app directory with bokeh server"""
-    Popen(["bokeh", "serve", "simple-annotator", "--port=8890", "--allow-websocket-origin=*"])
-    Popen(["bokeh", "serve", "linked-annotator", "--port=8891", "--allow-websocket-origin=*"])
+    Popen([
+        "bokeh",
+        "serve",
+        "simple-annotator",
+        "linked-annotator",
+        "--allow-websocket-origin=*",
+    ])
