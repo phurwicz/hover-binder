20ddd657663fa7696626072b9fe4f784581dcd8a
Pavel <74003834+phurwicz@users.noreply.github.com>
2020-11-19 12:20:07 +0800

    Caching heavy intermediates

--- local_lib/__init__.py
+++ local_lib/__init__.py
@@ -1,10 +1,12 @@
 import os
+import wrappy
 import hover
 from hover.utils.datasets import newsgroups_dictl
 from hover.core.dataset import SupervisableTextDataset
 
 
 # define functions that provide crucial intermediates
+@wrappy.memoize(cache_limit=10)
 def load_vectorizer(module_name):
     from importlib import import_module
 
@@ -12,6 +14,7 @@ def load_vectorizer(module_name):
     return module.get_vectorizer()
 
 
+@wrappy.memoize(cache_limit=10, return_copy=True)
 def create_embedded_dataset(module_name):
     data_home = os.path.join(os.path.dirname(__file__), "../scikit_learn_data/")
     my_20ng, label_encoder, label_decoder = newsgroups_dictl(data_home=data_home)
